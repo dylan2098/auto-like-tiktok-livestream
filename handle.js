cab11dae82980ba3d29564782f3ae3d2ddb044f4
Tri Nguyen <triminh.nguyen2098@gmail.com>
2024-08-20 21:40:46 +0700

    update text

--- handle.js
+++ handle.js
@@ -1,5 +1,5 @@
 chrome.storage.local.get('optionEnabled', function (result) {
-  $('#toggleButton').html(result.optionEnabled ? 'Turn Off' : 'Turn On');
+  $('#toggleButton').html(result.optionEnabled ? 'TURN OFF' : 'TURN ON');
 
   // set badget text
   chrome.action.setBadgeText({ text: result.optionEnabled ? 'ON' : 'OFF' });
@@ -9,7 +9,7 @@ $('#toggleButton').on('click', function () {
   chrome.tabs.reload(function(){});
   chrome.storage.local.get('optionEnabled', function (result) {
       chrome.storage.local.set({ optionEnabled: !(result.optionEnabled) }, function () {
-        $('#toggleButton').html(result.optionEnabled ? 'Turn On' : 'Turn Off');
+        $('#toggleButton').html(result.optionEnabled ? 'TURN ON' : 'TURN OFF');
         chrome.action.setBadgeText({ text: result.optionEnabled ? 'OFF' : 'ON' });
       });
   })
